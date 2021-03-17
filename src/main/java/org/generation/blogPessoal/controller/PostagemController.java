832a4157d5bd66ec4fa080276b513caa03f6574d
Nicoli <nicoli1992@gmail.com>
2021-03-17 14:56:45 -0300

    estrutura do projeto spring

---
+++ src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,40 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.generation.blogPessoal.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable long id) {
+		return repository.findById(id)
+				.map(response -> ResponseEntity.ok(response))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+
+}
